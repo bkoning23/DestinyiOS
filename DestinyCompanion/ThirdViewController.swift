382208ee1264de996710352f3db5c4cccde52c31
Brendan Koning <brendankoning@brendans-mac-mini.local>
2015-11-26 21:34:50 -0500

    Added ThirdViewController

---
+++ DestinyCompanion/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  DestinyCompanion
+//
+//  Created by Brendan Koning on 11/26/15.
+//  Copyright © 2015 Brendan Koning. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Second")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
